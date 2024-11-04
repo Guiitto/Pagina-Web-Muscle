44aaa04fb609b9480befedbcde3252110a6b2d51
Guiitto <diego.espinal.de@gmail.com>
2024-11-03 20:25:25 -0500

    Ajuste rutinas_hombre, inclusion botones Ver video

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("menu--active");
 }
+
+function cargarRutina(){
+    alert("Funcion que recargara la pagina web segun ejercicio seleccionado")
+}
